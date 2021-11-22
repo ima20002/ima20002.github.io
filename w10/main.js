9e6073e9950e77cb9d83ffa1a6c6d30a0cb9aa80
ima20002 <77100505+ima20002@users.noreply.github.com>
2021-11-23 00:16:50 +0900

    Add files via upload

--- w10/main.js
+++ w10/main.js
@@ -1,7 +1,24 @@
-function validateForm() {
-    let x = document.forms["myForm"]["eMail"].value;
-    if (x == "") {
-      alert("Name must be filled out");
-      return false;
-    }
-  } 
+const email = document.getElementById("mail");
+
+email.addEventListener("input", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("e-mail address must be filled out");
+  } else {
+    email.setCustomValidity("");
+  }
+});
+/* setCustomValidity(message) adds a custom error message to the element */
+
+const number = document.getElementById("number");
+
+number.addEventListener("input", function (event) {
+  if (number.validity.rangeUnderflow) {
+    number.setCustomValidity("Please type a number more than 1")
+  }else if (number.validity.rangeOverflow) {
+    number.setCustomValidity("Please type a number less than 10")
+  } else{
+    number.setCustomValidity("Good");
+  }
+})
+/*rangeOverflow: Returns true if the value is greater than the maximum 
+rangeUnderflow: Returns true if the value is less than the minimum */
